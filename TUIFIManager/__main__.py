656c4744e0e1e0acdbd2a2ad3eab9326d50237b6
sigmanificient <edhyjox@gmail.com>
2023-01-04 00:44:15 +0100

    Rewrite main script

--- TUIFIManager/__main__.py
+++ TUIFIManager/__main__.py
@@ -1,63 +1,76 @@
-# -*- coding: utf-8 -*-
-# import sys                                               # TESTING: UNCOMMENT TO USE LOCAL PACKAGE (./__init__.py) | https://stackoverflow.com/a/25888670/11465149
-# from os.path import dirname, abspath                     # TESTING: UNCOMMENT TO USE LOCAL PACKAGE (./__init__.py) | https://stackoverflow.com/a/25888670/11465149
-# sys.path.insert(0, dirname(dirname(abspath(__file__))))  # TESTING: UNCOMMENT TO USE LOCAL PACKAGE (./__init__.py) | https://stackoverflow.com/a/25888670/11465149
-# sys.path.append('..')                                               # TESTING WITH DAP
-# sys.path.append('/home/xou/.local/lib/python3.10/site-packages/')   # TESTING WITH DAP
+"""Entry point for TUIFIManager."""
+from __future__ import annotations
+
 import sys
 import unicurses as uc
+
 from TUIFIManager import TUIFIManager, BEGIN_MOUSE, END_MOUSE, __version__
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from ctypes import c_void_p
+    from typing import Final
+
+    Screen = c_void_p
+
+ESCAPE_KEY: Final[int] = 27
 
 
-def main():
-    global stdscr
+def setup_curses_screen() -> Screen:
+    std_scr = uc.initscr()
+    uc.start_color()
+    uc.cbreak()
+    uc.noecho()
+    uc.curs_set(0)
 
-    if "-v" in sys.argv or "--version" in sys.argv:
-        print(f"TUIFIManager v{__version__}")
-        return
+    # Initializing Mouse and then Update/refresh() std screen
+    uc.mouseinterval(0)
+    uc.mousemask(uc.ALL_MOUSE_EVENTS | uc.REPORT_MOUSE_POSITION)
+    uc.keypad(std_scr, True)
+    uc.nodelay(std_scr, False)
 
-    stdscr = uc.initscr()              # Global UniCurses Variable
-    event  = -1
+    # Initializing mouse movement | Don't move it above because it won't work on Windows
+    print(BEGIN_MOUSE)
+    uc.refresh()
+    return std_scr
 
-    uc.start_color  ( )
-    uc.cbreak       ( )
-    uc.noecho       ( )
-    uc.curs_set     (0)
-    uc.mouseinterval(0)                 # Initializing Mouse and then Update/refresh() stdscr
-    uc.mousemask    (uc.ALL_MOUSE_EVENTS | uc.REPORT_MOUSE_POSITION) # print("\033[?1003h\n")
-    uc.keypad       (stdscr, True )
-    uc.nodelay      (stdscr, False)
-    print           (BEGIN_MOUSE)       # Initializing mouse movement | Don't move it above because it won't work on Windows
-    uc.refresh      ( )
 
-    # Initializing TUIFIManager
-    HEIGHT,WIDTH       = uc.getmaxyx(stdscr)
-    starting_directory = sys.argv[1] if len(sys.argv) > 1 else '.'
-    fileManager        = TUIFIManager(0,0, HEIGHT,WIDTH, (True,True,True,True), starting_directory,suffixes=['*'], is_focused=True)
-    fileManager.refresh()
-
-    while event != 27 or fileManager.escape_event_consumed: # Main loop exit at event/(ch)aracter 27 = ESC if not fileManager.escape_event_consumed
-        event = uc.get_wch()
-        fileManager.handle_events(event)
-        fileManager.refresh()
-        if event == uc.KEY_RESIZE:
-            uc.resize_term(0,0)
-    print (END_MOUSE)
+def close_curses_screen():
+    print(END_MOUSE)
     uc.endwin()
 
 
-if __name__ == "__main__":
-    main()
+def start_tuifi():
+    std_scr = setup_curses_screen()
+    height, width = uc.getmaxyx(std_scr)
+    starting_directory = sys.argv[1] if len(sys.argv) > 1 else '.'
+    file_manager = TUIFIManager(
+        height=height, width=width,
+        anchor=(True, True, True, True),
+        directory=starting_directory,
+        suffixes=['*'],
+        is_focused=True
+    )
+
+    run_tuifi(file_manager)
+    close_curses_screen()
 
 
-# https://askubuntu.com/questions/17299/what-do-the-different-colors-mean-in-ls
-# https://stackoverflow.com/questions/11753909/clean-up-ncurses-mess-in-terminal-after-a-crash
-# https://stackoverflow.com/questions/35336532/ncurses-subwin-or-subpad-of-a-pad
-# https://stackoverflow.com/questions/31488362/why-is-dict-faster-than-if-else-in-python
-# https://stackoverflow.com/questions/18166977/cd-to-dir-after-exiting-script-system-independent-way-purely-in-python
+def run_tuifi(file_manager: TUIFIManager):
+    event: int = -1
 
-# TODO:
-"""
-- Scroll sensitivity
-- TERMUX start from bottom (because most mobile screens are too big)
-"""
+    file_manager.refresh()
+    while event != ESCAPE_KEY or file_manager.escape_event_consumed:  
+        event = uc.get_wch()
+        file_manager.handle_events(event)
+        file_manager.refresh()
+
+        if event == uc.KEY_RESIZE:
+            uc.resize_term(0, 0)
+
+
+def main():
+    if "-v" in sys.argv or "--version" in sys.argv:
+        print(f"TUIFIManager v{__version__}")
+    else:
+        start_tuifi()
