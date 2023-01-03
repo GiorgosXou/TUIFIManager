6bd1572bd0c63b5553aba8e8bdcf83ce71d45228
sigmanificient <edhyjox@gmail.com>
2023-01-03 23:50:57 +0100

    Add setup.cfg

--- setup.py
+++ setup.py
@@ -1,33 +1,4 @@
 from setuptools import setup
-import sys
 
-
-if sys.version_info.major < 3:
-    sys.exit('Python < 3 is unsupported (for now).')
-    
-
-#def readfile(filename):
-#    with open(filename, 'r+') as f:
-#        return f.read()
-
-
-setup(
-    name="TUIFIManager",
-    version="2.1.9",
-    description="A cross-platform terminal-based termux-oriented file manager (and component), meant to be used with a Uni-Curses project or as is. This project is mainly an attempt to get more attention to the Uni-Curses project.",
-    #long_description=readfile('README.md'),
-    author="George Chousos",
-    author_email="gxousos@gmail.com",
-    url="https://github.com/GiorgosXou/TUIFIManager",
-    packages=['TUIFIManager'],
-    install_requires=['Uni-Curses>=2.1.3', 'Send2Trash==1.8.0'],
-    entry_points={
-        'console_scripts': [
-            'tuifi = TUIFIManager.__main__:main'
-        ]
-    },
-)
-
-# pip3 install .
-# python setup.py sdist
-# twine upload dist/*
+if __name__ == '__main__':
+    setup()
