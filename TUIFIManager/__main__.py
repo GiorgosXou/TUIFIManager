78c66e2cb4259695d04bacd865308c0f5c5a1078
sigmanificient <edhyjox@gmail.com>
2023-01-05 20:06:13 +0100

    Improve __main__ script

--- TUIFIManager/__main__.py
+++ TUIFIManager/__main__.py
@@ -6,49 +6,77 @@ from __future__ import annotations
 # sys.path.append('..')                                               # TESTING WITH DAP
 # sys.path.append('/home/xou/.local/lib/python3.10/site-packages/')   # TESTING WITH DAP
 import sys
+from typing import Final, TYPE_CHECKING
+
 import unicurses as uc
 from TUIFIManager import TUIFIManager, BEGIN_MOUSE, END_MOUSE, __version__
 
+if TYPE_CHECKING:
+    from ctypes import c_void_p
+
+    Screen = c_void_p
+
 ESCAPE_KEY = 27
+EXIT_SUCCESS: Final[int] = 0
 
 
-def main():
-    global stdscr
-    stdscr = uc.initscr()              # Global UniCurses Variable
-    event  = -1
+def init_tui() -> Screen:
+    std_screen = uc.initscr()              # Global UniCurses Variable
 
     uc.start_color  ( )
     uc.cbreak       ( )
     uc.noecho       ( )
     uc.curs_set     (0)
-    uc.mouseinterval(0)                 # Initializing Mouse and then Update/refresh() stdscr
+    uc.mouseinterval(0)                 # Initializing Mouse and then Update/refresh() the screen
     uc.mousemask    (uc.ALL_MOUSE_EVENTS | uc.REPORT_MOUSE_POSITION) # print("\033[?1003h\n")
-    uc.keypad       (stdscr, True )
-    uc.nodelay      (stdscr, False)
+    uc.keypad       (std_screen, True)
+    uc.nodelay      (std_screen, False)
     print           (BEGIN_MOUSE)       # Initializing mouse movement | Don't move it above because it won't work on Windows
     uc.refresh      ( )
 
-    # Initializing TUIFIManager
-    HEIGHT,WIDTH       = uc.getmaxyx(stdscr)
-    starting_directory = sys.argv[1] if len(sys.argv) > 1 else '.'
-    fileManager        = TUIFIManager(0,0, HEIGHT,WIDTH, (True,True,True,True), starting_directory,suffixes=['*'], is_focused=True)
-    fileManager.refresh()
+    return std_screen
 
-    while event != ESCAPE_KEY or fileManager.escape_event_consumed: # Main loop 
+
+def run_file_manager(file_manager: TUIFIManager, std_screen: Screen):
+    event = -1
+
+    file_manager.refresh()
+    while event != ESCAPE_KEY or file_manager.escape_event_consumed: # Main loop
         event = uc.get_wch()
-        fileManager.handle_events(event)
-        fileManager.refresh()
+
+        file_manager.handle_events(event)
+        file_manager.refresh()
+
         if event == uc.KEY_RESIZE:
-            uc.resize_term(0,0)
+            uc.resize_term(0, 0)
+
+
+def run_tui():
+    std_screen = init_tui()
+    height , width       = uc.getmaxyx(std_screen)
+    starting_directory = sys.argv[1] if len(sys.argv) > 1 else '.'
+    fileManager        = TUIFIManager(
+        height=height, width=width,
+        anchor=(True, True, True, True),
+        directory=starting_directory,
+        suffixes=['*'], is_focused=True
+    )
+
+    run_file_manager(fileManager, std_screen)
+
     print(END_MOUSE)
     uc.endwin()
 
-
-if __name__ == "__main__":
+def main() -> int:
     if "-v" in sys.argv or "--version" in sys.argv:
         print(f"TUIFIManager v.{__version__}")
-        exit()
-    main()
+    else:
+        run_tui()
+    return EXIT_SUCCESS
+
+
+if __name__ == "__main__":
+    sys.exit(main())
 
 
 # https://askubuntu.com/questions/17299/what-do-the-different-colors-mean-in-ls
