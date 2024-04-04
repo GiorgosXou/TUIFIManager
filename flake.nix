d3b140beddde1846b7178a800535414e1f0eff0e
Sigmanificient <edhyjox@gmail.com>
2024-04-04 05:27:12 +0200

    Remove unsupported flake system

--- flake.nix
+++ flake.nix
@@ -11,13 +11,7 @@
     nixpkgs,
     flake-utils,
   }:
-    flake-utils.lib.eachSystem [
-      "aarch64-darwin"
-      "aarch64-linux"
-      "x86_64-darwin"
-      "x86_64-linux"
-    ]
-    (system: let
+    flake-utils.lib.eachSystem ["x86_64-linux"] (system: let
       pkgs = nixpkgs.legacyPackages.${system};
     in {
       devShells.default = let
